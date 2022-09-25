415574cd38f96fcb19fa9e36c54fb35a028f1e4a
krusty <krusty@savla.dev>
2022-09-25 22:28:05 +0200

    fix go-vet errors

--- models.go
+++ models.go
@@ -15,14 +15,14 @@ type Map struct {
 
 type MapMeta struct {
 	Name       string `json:"map_name"`
-	Dimensions int8   `json:"map_dimensions" default:'2'`
+	Dimensions int8   `json:"map_dimensions"`
 	RoomCount  int8   `json:"room_count"`
 }
 
 type Room struct {
 	Description string   `json:"description"`
 	North       string   `json:"north"`
-	South       string   `json:south`
+	South       string   `json:"south"`
 	West        string   `json:"west"`
 	East        string   `json:"east"`
 	Items       []string `json:"items"`
@@ -38,5 +38,5 @@ type Room struct {
 type Effect struct {
 	Type         string `json:"type"`
 	RequiredItem string `json:"required-item"`
-	ShowHidden   bool   `json:"show-hidden" default:false`
+	ShowHidden   bool   `json:"show-hidden"`
 }
